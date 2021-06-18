d0eaa868394faf8f6e7bf01dea31e1f0f9e65bf0
wellington barbosa <carneirobarbosawellington@gmail.com>
2021-06-18 07:13:43 -0300

    Generate book/author routes and controller methods: '

---
+++ app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show($book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($book)
+    {
+        //
+    }
+}
